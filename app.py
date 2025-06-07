b08034f73dcbf8c24d3d5eb473d791f3fc778137
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-08 00:32:00 +0100

    xxx

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import cv2
+import numpy as np
+import base64
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html', title='Welcome', message='Hello from Flask!')
+ 
+@app.route('/capture', methods=['POST'])
+def capture():
+    data_url = request.json['image']
+    header, encoded = data_url.split(",", 1)
+    img_data = base64.b64decode(encoded)  
+
+    # Convert bytes to NumPy array and decode with OpenCV
+    nparr = np.frombuffer(img_data, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    cv2.imwrite(CAPTURED_IMAGE, img) # type: ignore
+
+    return jsonify({'status': 'success', 'path': f'/{CAPTURED_IMAGE}'}) # type: ignore
+ 
 
 @app.route('/attendance')
 def attendance():
