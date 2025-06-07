da4d36254659af538ce3129888d36d3c0f3a2aa9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-06-07 23:52:42 +0100

    hhh

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('index.html', title='Welcome', message='Hello from Flask!')
 
+@app.route('/attendance')
+def attendance():
+    return render_template('attendance.html', title='Welcome', message='Hello from Attendance!')
+
+@app.route('/students')
+def students():
+    return render_template('students.html', title='Welcome', message='Hello from Students!')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', title='Welcome', message='Hello from Dashboard!')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Welcome', message='Hello from Login!')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=True)
